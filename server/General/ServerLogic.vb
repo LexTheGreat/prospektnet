9457ebc4be47b13600696b4275f5d643a7dbc82a
Lex <lexkymbeth@gmail.com>
2013-10-31 19:22:05 -0400

    MD5 Register and login
    Why did you not do this before.... D: MD5 ALL THE WAY BRO

--- server/General/ServerLogic.vb
+++ server/General/ServerLogic.vb
@@ -1,5 +1,8 @@
-﻿Public Module ServerLogic
-    Public Function ResizeArray(arr As Array, newSizes() As Integer) As Array
+﻿Imports System.Text
+Imports System.Security.Cryptography
+
+Public Module ServerLogic
+    Public Function ResizeArray(ByVal arr As Array, ByVal newSizes() As Integer) As Array
         If newSizes.Length <> arr.Rank Then
             Throw New ArgumentException()
         End If
@@ -8,5 +11,18 @@
         Dim length As Integer = If(arr.Length <= temp.Length, arr.Length, temp.Length)
         Array.ConstrainedCopy(arr, 0, temp, 0, length)
         Return temp
+    End Function
+    Function Md5FromString(ByVal Source As String) As String
+        Dim Bytes() As Byte
+        Dim sb As New StringBuilder()
+        If String.IsNullOrEmpty(Source) Then
+            Throw New ArgumentNullException
+        End If
+        Bytes = Encoding.Default.GetBytes(Source)
+        Bytes = MD5.Create().ComputeHash(Bytes)
+        For x As Integer = 0 To Bytes.Length - 1
+            sb.Append(Bytes(x).ToString("x2"))
+        Next
+        Return sb.ToString()
     End Function
 End Module
